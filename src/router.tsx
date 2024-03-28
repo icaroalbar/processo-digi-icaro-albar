e4817ed6b3ebdc84e53a728cd119d7190521974c
Icaro Albar <icaro.albar@live.com>
2024-03-28 18:48:09 -0300

    :bug: fix: router home

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Product from "./pages/product";
+import Home from "./pages/home";
 
 export function Router() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/product/:id" element={<Product />} />
       </Routes>
     </BrowserRouter>
