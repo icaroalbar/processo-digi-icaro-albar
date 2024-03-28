275a8d1b4dbe62aa3cc98e41486c2ea80f5f1444
Icaro Albar <icaro.albar@live.com>
2024-03-28 14:30:24 -0300

    :sparkles: feat: function addToCart created

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,9 +1,12 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useFetch } from "@/hooks";
 import Icon from "@/components/icon";
 import { Button } from "@/components/ui/button";
 import ImagemProduct from "./partials/imagem-product";
 import ProductDetails from "./partials/product-details";
+import { Toaster } from "@/components/ui/toaster";
+import axios from "axios";
+import { toast } from "@/components/ui/use-toast";
 
 export default function Product() {
   const { id } = useParams();
@@ -12,8 +15,24 @@ export default function Product() {
   if (error) return <div>Erro ao buscar dados.</div>;
   if (!data) return <div>Carregando...</div>;
 
+  const addToCart = async (data: unknown) => {
+    await axios.post("http://localhost:3002/cart", data);
+    toast({
+      title: "Produto adicionado!",
+      description: "Clique no carrinho ao lado para finalizar suas compras.",
+      action: (
+        <Button variant={"outline"} asChild>
+          <Link to="/">
+            <Icon name="ShoppingCart" />
+          </Link>
+        </Button>
+      ),
+    });
+  };
+
   return (
     <div className="container pb-16 md:h-screen">
+      <Toaster />
       <div className="container grid grid-cols-2 gap-4 py-10">
         <ImagemProduct image={data.image} offer={data.offer} />
         <div className="col-span-2 flex flex-col justify-between md:col-span-1">
@@ -24,7 +43,10 @@ export default function Product() {
             price={data.price}
             info={data.info}
           />
-          <Button className="flex items-center gap-x-3">
+          <Button
+            onClick={() => addToCart(data)}
+            className="flex items-center gap-x-3"
+          >
             <Icon name="ShoppingCart" />
             Adicionar ao carrinho
           </Button>
