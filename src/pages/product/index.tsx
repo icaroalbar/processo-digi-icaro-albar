aa5492dfb7e1e0614ac384b3aadd060c8d9c0ffc
Icaro Albar <icaro.albar@live.com>
2024-03-28 11:28:01 -0300

    :recycle: refactor: partials page product

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,8 +1,9 @@
 import { useParams } from "react-router-dom";
 import { useFetch } from "@/hooks";
 import Icon from "@/components/icon";
-import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import ImagemProduct from "./partials/imagem-product";
+import ProductDetails from "./partials/product-details";
 
 export default function Product() {
   const { id } = useParams();
@@ -14,52 +15,19 @@ export default function Product() {
   return (
     <div className="container">
       <div className="container grid grid-cols-2 gap-4 py-10">
-        <div className="col-span-2 flex justify-center md:col-span-1">
-          <img
-            src={
-              data.offer === "HERO"
-                ? "http://placehold.it/300x300/999/CCC"
-                : data.image
-            }
-            width={0}
-            height={0}
-            alt="Imagem illustrativa"
-            className="w-3/4 rounded-lg"
+        <ImagemProduct image={data.image} />
+        <div className="col-span-2 flex flex-col justify-between md:col-span-1">
+          <ProductDetails
+            id={data.id}
+            name={data.name}
+            offer={data.offer}
+            price={data.price}
+            info={data.info}
           />
-        </div>
-        <div className="col-span-2 md:col-span-1">
-          <p className="text-end text-sm capitalize text-muted-foreground">
-            código: {data.id}
-          </p>
-          <h2 className="mb-2 scroll-m-20 border-b pb-2 text-3xl font-semibold uppercase tracking-tight first:mt-0">
-            {data.name}
-          </h2>
-          <Badge className={`${!data.offer && "hidden"}`}>
-            {data.offer === "HERO" ? (
-              "Lançamento"
-            ) : (
-              <>
-                <Icon name="ArrowDown" size={16} /> 25%
-              </>
-            )}
-          </Badge>
-          <div className="mb-4 flex gap-x-4">
-            <h1 className="flex scroll-m-20 items-start pt-6 text-4xl font-semibold tracking-tight lg:text-5xl">
-              <span className="pr-2 pt-1 text-lg">R$</span>
-              {data.price}
-            </h1>
-            <h1
-              className={`flex scroll-m-20 items-start pt-6 text-4xl font-medium tracking-tight text-muted line-through lg:text-5xl ${!data.offer && "hidden"} ${data.offer === "HERO" && "hidden"}`}
-            >
-              <span className="pr-2 pt-1 text-lg">R$</span>
-              {(data.price * 1.25).toFixed(2)}
-            </h1>
-          </div>
           <Button className="flex items-center gap-x-3">
             <Icon name="ShoppingCart" />
             Adicionar ao carrinho
           </Button>
-          <p className="leading-7 [&:not(:first-child)]:mt-6">{data.info}</p>
         </div>
       </div>
       <div>
