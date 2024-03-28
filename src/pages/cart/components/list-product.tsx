954263e9296d760699e351a02230d4da25f000c0
Icaro Albar <icaro.albar@live.com>
2024-03-28 17:36:48 -0300

    :recycle: refactor: page-cart

---
+++ src/pages/cart/components/list-product.tsx
@@ -0,0 +1,64 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import Icon from "@/components/icon";
+
+interface ListProductProps {
+  id: string;
+  image: string;
+  offer?: string;
+  name: string;
+  price: string;
+  totalQuantity: () => void;
+}
+
+export default function ListProduct({
+  id,
+  image,
+  offer,
+  name,
+  price,
+  totalQuantity,
+}: ListProductProps) {
+  return (
+    <Card
+      className="grid grid-cols-3 items-center justify-between rounded-none border-x-transparent border-t-transparent md:flex"
+      key={id}
+    >
+      <CardContent className="max-w-44">
+        <img
+          src={offer === "HERO" ? "http://placehold.it/300x300/999/CCC" : image}
+          width={0}
+          height={0}
+          alt="Imagem illustrativa"
+          className="w-3/4 rounded-lg"
+        />
+      </CardContent>
+      <CardContent>
+        <CardTitle className="capitalize">{name}</CardTitle>
+        <CardDescription className="capitalize">código {id}</CardDescription>
+      </CardContent>
+      <CardContent>
+        <CardTitle>Quantidade</CardTitle>
+        <CardDescription>{totalQuantity}</CardDescription>
+      </CardContent>
+      <CardContent>
+        <CardTitle>Valor unitário</CardTitle>
+        <CardDescription>R${price}</CardDescription>
+      </CardContent>
+      <CardContent>
+        <CardTitle>Valor total</CardTitle>
+        <CardDescription>R${price * totalQuantity}</CardDescription>
+      </CardContent>
+      <CardContent>
+        <Button variant={"ghost"} size={"icon"}>
+          <Icon name="Trash2" size={18} />
+        </Button>
+      </CardContent>
+    </Card>
+  );
+}
--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,20 +1,14 @@
 import { useFetch } from "@/hooks";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardTitle,
-} from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
+import ListProduct from "./components/list-product";
 
 export default function Cart() {
   const { data, error } = useFetch(`http://localhost:3002/cart/`);
   if (error) return <div>Erro ao buscar dados.</div>;
   if (!data) return <div>Carregando...</div>;
 
-  // Filtrar os dados para remover produtos duplicados com base no ID
-  const uniqueProducts = [];
-  const uniqueProductIds = [];
+  const uniqueProducts: any[] = [];
+  const uniqueProductIds: any[] = [];
   data.forEach((product: any) => {
     if (!uniqueProductIds.includes(product.id)) {
       uniqueProductIds.push(product.id);
@@ -22,7 +16,6 @@ export default function Cart() {
     }
   });
 
-  // Calcular o valor total do carrinho
   let totalPrice = 0;
   uniqueProducts.forEach((product: any) => {
     const totalQuantity = data.filter((p: any) => p.id === product.id).length;
@@ -32,50 +25,20 @@ export default function Cart() {
   return (
     <section className="container flex flex-col gap-y-3 py-10">
       {uniqueProducts.map((product: any) => {
-        // Calcular a quantidade total de cada produto no carrinho
         const totalQuantity = data.filter(
           (p: any) => p.id === product.id,
         ).length;
 
         return (
-          <Card
-            className="flex items-center justify-between rounded-none border-x-transparent border-t-transparent"
+          <ListProduct
+            id={product.id}
+            image={product.image}
+            name={product.name}
+            price={product.price}
+            totalQuantity={totalQuantity}
             key={product.id}
-          >
-            <CardContent className="max-w-44">
-              <img
-                src={
-                  product.offer === "HERO"
-                    ? "http://placehold.it/300x300/999/CCC"
-                    : product.image
-                }
-                width={0}
-                height={0}
-                alt="Imagem illustrativa"
-                className="w-3/4 rounded-lg"
-              />
-            </CardContent>
-            <CardContent>
-              <CardTitle className="capitalize">{product.name}</CardTitle>
-              <CardDescription className="capitalize">
-                código {product.id}
-              </CardDescription>
-            </CardContent>
-            <CardContent>
-              <CardTitle>Quantidade</CardTitle>
-              <CardDescription>{totalQuantity}</CardDescription>
-            </CardContent>
-            <CardContent>
-              <CardTitle>Valor unitário</CardTitle>
-              <CardDescription>R${product.price}</CardDescription>
-            </CardContent>
-            <CardContent>
-              <CardTitle>Valor total</CardTitle>
-              <CardDescription>
-                R${product.price * totalQuantity}
-              </CardDescription>
-            </CardContent>
-          </Card>
+            offer={product.offer}
+          />
         );
       })}
       <div className="flex items-center justify-around">
