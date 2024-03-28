0808cfcb328c0510d71256bd741fb76eaf50b344
Icaro Albar <icaro.albar@live.com>
2024-03-28 15:29:53 -0300

    :sparkles: feat: page cart

--- src/router.tsx
+++ src/router.tsx
@@ -1,13 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/home";
-import Product from "./pages/product";
+import Cart from "./pages/cart";
 
 export function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/product/:id" element={<Product />} />
+        <Route path="/cart/" element={<Cart />} />
       </Routes>
     </BrowserRouter>
   );
