549c7a7542334a41cfe7338378ec11670a1aaa32
Icaro Albar <icaro.albar@live.com>
2024-03-29 14:52:33 -0300

    :bug: conflit router

--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-<<<<<<< HEAD
 import Product from "./pages/product";
 import Home from "./pages/home";
-=======
-import Home from "./pages/home";
-import Product from "./pages/product";
 import Cart from "./pages/cart";
->>>>>>> 5ecf89cce3c42b27884e4b3d5d491e41205af041
 
 export function Router() {
   return (
@@ -14,10 +9,7 @@ export function Router() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/product/:id" element={<Product />} />
-<<<<<<< HEAD
-=======
         <Route path="/cart" element={<Cart />} />
->>>>>>> 5ecf89cce3c42b27884e4b3d5d491e41205af041
       </Routes>
     </BrowserRouter>
   );
